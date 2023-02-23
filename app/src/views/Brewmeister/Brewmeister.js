e63e1c3063c3e286e0ee0c194a738c598975787c
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-22 20:50:05 -0500

    mock brewmeister  page

--- app/src/views/Brewmeister/Brewmeister.js
+++ app/src/views/Brewmeister/Brewmeister.js
@@ -1,1 +1,12 @@
 import './Brewmeister.css'
+
+
+function Brewmeister() {
+    return (
+        <div className='brewmeister'>
+            <h1>this is the brewmeister page</h1>
+        </div>
+    );
+}
+
+export default Brewmeister;
