26ef42a149b16a7639c45c4776053baab1fe6790
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-22 20:49:19 -0500

    mock home page

--- app/src/views/Home/Home.js
+++ app/src/views/Home/Home.js
@@ -1,1 +1,12 @@
 import './Home.css'
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>this is the home page</h1>
+        </div>
+    );
+}
+
+export default Home;
