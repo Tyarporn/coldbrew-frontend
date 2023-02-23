9e1d5d3a857468e06584cf35c92302a92b768a80
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-22 20:51:37 -0500

    mock navBar  page

--- app/src/components/NavigationBar/NavBar.js
+++ app/src/components/NavigationBar/NavBar.js
@@ -1,1 +1,13 @@
 import './NavBar.css'
+
+
+
+function NavBar() {
+    return (
+        <div className='navbar'>
+            <h1>this is the navigation bar</h1>
+        </div>
+    );
+}
+
+export default NavBar;
