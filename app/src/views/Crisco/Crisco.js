7eb0223a09ed5f3e9e2ea71cd7c82393fd0ddeb6
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-22 20:49:39 -0500

    mock crisco page

---
+++ app/src/views/Crisco/Crisco.js
@@ -0,0 +1,12 @@
+import './Crisco.css'
+
+
+function Crisco() {
+    return (
+        <div className='crisco'>
+            <h1>this is the crisco page</h1>
+        </div>
+    );
+}
+
+export default Crisco;
