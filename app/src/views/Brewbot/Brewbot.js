e43b5219dd4d4eba15bf5f5f8fd7894c77b6877e
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-22 20:50:26 -0500

    mock brewbot  page

--- app/src/views/Brewbot/Brewbot.js
+++ app/src/views/Brewbot/Brewbot.js
@@ -1,1 +1,12 @@
 import './Brewbot.css'
+
+
+function Brewbot() {
+    return (
+        <div className='brewbot'>
+            <h1>this is the brewbot page</h1>
+        </div>
+    );
+}
+
+export default Home;
