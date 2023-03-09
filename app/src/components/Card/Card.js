ff0023ea9d9762c5bc38a4cbf10162ec20c29532
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-08 22:37:49 -0500

    created card.js

---
+++ app/src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import './Card.css'
+import React, { Component } from "react";
+
+function Card() {
+    return (
+        <div className='card'>
+        <div className='row product'>
+            <div className="middle-area">
+                <button className='btn'>BrewMeister</button>
+                <button className='btn'>BrewBot</button>
+                <button className='btn'>Crisco</button>
+                <button className='btn'>Stonkster</button>
+            </div>
+            <div className="symbols">
+                <img src='BrewBotPic.png' alt="Sample Image" height="170" width="250"/>
+            </div>
+        </div>
+        
+        </div>
+    );
+}
+
+
+export default Card;
