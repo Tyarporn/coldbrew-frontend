4d20b4a10489e91dd8ddf66a62371464f2af11da
Cameron Vong <cameronvong@gmail.com>
2023-03-23 13:36:26 -0400

    Changed website description on homepage

--- app/src/views/Home/Home.js
+++ app/src/views/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 function Home() {
     return (
         <div className='home'>
-            <h1>this is the home page</h1>
+            <h1>BrewBots is the premier website to discover and share Discord bots</h1>
         </div>
     );
 }
