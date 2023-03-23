02fdc9eab1814f30085822f86e42735f16daa110
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-23 16:03:39 -0400

    boilerplate contact us js code

---
+++ app/src/views/Contact/Contact.js
@@ -0,0 +1,13 @@
+import './Contact.css'
+import React, { Component } from "react";
+
+
+function Contact() {
+    return (
+        <div className='contact'>
+            <h1>this is the contact us page</h1>
+        </div>
+    );
+}
+
+export default Contact;
