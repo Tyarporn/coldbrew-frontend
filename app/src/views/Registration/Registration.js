4130e08287dbe8b333d73cc66eacf54a7cc80724
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-28 13:57:51 -0400

    added buttons standard for registration form

--- app/src/views/Registration/Registration.js
+++ app/src/views/Registration/Registration.js
@@ -4,10 +4,24 @@ import React from 'react'
 
 function Registration() {
     return (
-        <div className='login'>
-            <h1>Registration page</h1>
+        <div className='sign-up-form'>
+            <h1>Register Now</h1>
+            <form>
+                <label>Please Enter Your Email</label>
+                <input type="email" className="input-box" placeholder="Your Email"></input>
+                <label>Please Enter Your Password</label>
+                <input type="password" className="input-box" placeholder="Your Password"></input>
+                <label>Please Re-Enter Your Password</label>
+                <input type="password" className="input-box" placeholder="Your Password"></input>
+                <button type="button" className="signup-btn">Create Account</button>
+                <hr/>
+                <p>Already Have an Account?</p>
+                <a href={'http://localhost:3000/login'} >
+                        <button onClick type='button' className='signup-btn'>Login</button>
+                </a>
+            </form>
         </div>
-    );
+    )
 }
 
 export default Registration;
