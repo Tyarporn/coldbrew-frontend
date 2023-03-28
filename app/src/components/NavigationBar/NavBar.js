16f8968482196bc88f76c344261d4b2d79f919ae
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-28 19:45:40 -0400

    added hateoas state code

--- app/src/components/NavigationBar/NavBar.js
+++ app/src/components/NavigationBar/NavBar.js
@@ -1,7 +1,26 @@
 import './NavBar.css'
 import React from 'react'
+import {useState, useEffect} from 'react';
+import axios from "axios"
 
 function NavBar() {
+
+    const [mainMenu, setMainMenu] = useState([])
+    const fetchHATEOAS = async () => {
+        return axios.get("http://127.0.0.1:8080/main_menu")
+        .then(res => {
+            console.log(res)
+            setBotName(res.data.Data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }
+    
+      useEffect(() => {
+        fetchHATEOAS()
+      }, [])
+
     return (
         <div className="container">
             <div className="header-area">
