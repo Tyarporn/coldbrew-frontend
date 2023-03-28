b3ea86de74629ec89cc466b90250487f09d85fd7
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-28 13:40:11 -0400

    created registration pages

---
+++ app/src/views/Registration/Registration.js
@@ -0,0 +1,13 @@
+import './Registration.css'
+import React from 'react'
+
+
+function Registration() {
+    return (
+        <div className='login'>
+            <h1>Registration page</h1>
+        </div>
+    );
+}
+
+export default Registration;
