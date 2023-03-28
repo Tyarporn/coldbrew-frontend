7656fadc46ad55c0d3091172148a575f7002da1c
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-28 13:52:43 -0400

    added html code for login.js

--- app/src/views/Login/Login.js
+++ app/src/views/Login/Login.js
@@ -4,10 +4,22 @@ import React from 'react'
 
 function Login() {
     return (
-        <div className='login'>
-            <h1>Login page</h1>
+        <div className='sign-up-form'>
+            <h1>Login</h1>
+            <form>
+                <label>Please Enter Your Email</label>
+                <input type="email" className="input-box" placeholder="Your Email"></input>
+                <label>Please Enter Your Password</label>
+                <input type="password" className="input-box" placeholder="Your Password"></input>
+                <button type="button" className="signup-btn">Login</button>
+                <hr/>
+                <p>Don't Have an Account? Register Now!</p>
+                <a href={'http://localhost:3000/register'} >
+                        <button onClick type='button' className='signup-btn'>Register</button>
+                    </a>
+            </form>
         </div>
-    );
+    )
 }
 
 export default Login;
