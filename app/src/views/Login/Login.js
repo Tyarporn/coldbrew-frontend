b74d8caaee783c003b5eeb8b139d1240d27c3609
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-28 13:40:02 -0400

    created login pages

---
+++ app/src/views/Login/Login.js
@@ -0,0 +1,13 @@
+import './Login.css'
+import React from 'react'
+
+
+function Login() {
+    return (
+        <div className='login'>
+            <h1>Login page</h1>
+        </div>
+    );
+}
+
+export default Login;
