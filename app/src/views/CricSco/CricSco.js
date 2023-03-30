424b8eb38a3fe527aa48fac700500ad250aeba9c
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-03-30 15:42:25 -0400

    added usestate and useffect to crisco

--- app/src/views/CricSco/CricSco.js
+++ app/src/views/CricSco/CricSco.js
@@ -6,6 +6,51 @@ import axios from "axios"
 
 
 function CricSco() {
+
+    const [botName, setBotName] = useState([])
+    const [botID, setBotID] = useState([])
+    const [botDescription, setBotDescription] = useState([])
+    const images = ['brewmeister.png', 'brew_bot_logo.png', 'crisCo_logo.jpeg', 'stonkster_logo.png']
+
+    const fetchBotNames = async () => {
+        return axios.get("http://127.0.0.1:8080/listbots")
+        .then(res => {
+            console.log(res)
+            setBotName(res.data.Data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }
+
+      const fetchBotID = async () => {
+        return axios.get("http://127.0.0.1:8080/show_bot_ids")
+        .then(res => {
+            console.log(res)
+            setBotID(res.data.Data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }
+
+      const fetchBotDescription = async () => {
+        return axios.get("http://127.0.0.1:8080/show_bot_details")
+        .then(res => {
+            console.log(res)
+            setBotDescription(res.data.Data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }
+
+      useEffect(() => {
+        fetchBotNames()
+        fetchBotDescription()
+        fetchBotID()
+      }, [])
+      
     return (
         <div className='crisco'>
             <h1>this is the crisco page</h1>
