a16ab9cd6bb8b60681e8e5210822fd7524e5a6b4
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-03-31 15:55:13 -0400

    added url to menu choices

--- app/src/components/NavigationBar/NavBar.js
+++ app/src/components/NavigationBar/NavBar.js
@@ -4,33 +4,42 @@ import {useState, useEffect} from 'react';
 import axios from "axios"
 
 function NavBar() {
+    const [mainMenuA, setMainMenuA] = useState([])
+    const [mainMenuB, setMainMenuB] = useState([])
+    const [mainMenuC, setMainMenuC] = useState([])
+    const [mainMenuD, setMainMenuD] = useState([])
+    const [mainMenuE, setMainMenuE] = useState([])
 
-    // const [mainMenu, setMainMenu] = useState([])
-
-    // const fetchHATEOAS = async () => {
-    //     return axios.get("http://127.0.0.1:8080/main_menu")
-    //     .then(res => {
-    //         console.log(res)
-    //         setMainMenu(res.data.Choices)
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    //   }
+    const fetchHATEOAS = async () => {
+        return axios.get("http://127.0.0.1:8080/main_menu")
+        .then(res => {
+            console.log("res.data",res.data.Choices["b"])
+            setMainMenuA(res.data.Choices.a)
+            setMainMenuB(res.data.Choices.b)
+            setMainMenuC(res.data.Choices.c)
+            setMainMenuD(res.data.Choices.d)
+            setMainMenuE(res.data.Choices.e)
+            
+        })
+        .catch(err => {
+            console.log(err)
+        })
+      }
     
-    //   useEffect(() => {
-    //     fetchHATEOAS()
-    //   }, [])
+      useEffect(() => {
+        fetchHATEOAS()
+      }, [])
 
     return (
+        console.log("mainMenuA", mainMenuA.url),
         <div className="container">
             <div className="header-area">
             <ul class="links">
-                <li><a class="active" href='/'>Home</a></li>
-                <li><a href='/discover'>Discover</a></li>
-                <li><a href='/about'>About</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-                <li className='last_button'><a href="/login">Login</a></li>
+                <li><a class="active" href={mainMenuA.url}>Home</a></li>
+                <li><a href={mainMenuB.url}>Discover</a></li>
+                <li><a href={mainMenuC.url}>About</a></li>
+                <li><a href={mainMenuD.url}>Contact Us</a></li>
+                <li className='last_button'><a href={mainMenuE.url}>Login</a></li>
             </ul>
             </div>
         </div>
--- app/src/views/Home/Home.js
+++ app/src/views/Home/Home.js
@@ -6,35 +6,9 @@ import axios from "axios"
 
 
 function Home() {
-    const [mainMenu, setMainMenu] = useState([])
-
-    const fetchHATEOAS = async () => {
-        return axios.get("http://127.0.0.1:8080/main_menu")
-        .then(res => {
-            console.log("res.data",res.data.Choices["2"])
-            setMainMenu(res.data.Choices.a)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-      }
-    
-      useEffect(() => {
-        fetchHATEOAS()
-      }, [])
+ 
     return (
-        console.log("return1", mainMenu.url),
-        // console.log("return2", Object.values(Object.values(mainMenu)[2])),
         <div className='home'>
-            {/* <div>
-                {mainMenu.map(item => <div>{item}</div>)}
-            </div> */}
-            {/* <div>
-                {mainMenu.Choices['1']['url']}
-            </div> */}
-            {/* <section>
-                {Array.from({ length: 5 }, (_, i) => <div>{mainMenu.Choices}</div>)}
-            </section> */}
             <h2>Hi there!</h2>
             <p>Welcome to BrewBots</p>
             <p>The premier place to discover and distribute Discord bots.</p>
