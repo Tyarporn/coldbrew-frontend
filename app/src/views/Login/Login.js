5bb27a44d8d41f325b35bfbbb6cdd002f0b32ef8
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-04-03 14:31:39 -0400

    adding handlesubmit function

--- app/src/views/Login/Login.js
+++ app/src/views/Login/Login.js
@@ -1,8 +1,28 @@
 import './Login.css'
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const response = await axios.post('/api/login', { email, password });
+          const token = response.data.token;
+          localStorage.setItem('token', token);
+          // redirect to dashboard or homepage
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+
     return (
         <div className='sign-up-form'>
             <h1>Login</h1>
