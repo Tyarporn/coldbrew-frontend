d99fe7069ac825f0a51db2d6d9ded8ecaad3d109
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-04-05 11:35:17 -0400

    added html button support for login form

--- app/src/views/Login/Login.js
+++ app/src/views/Login/Login.js
@@ -26,16 +26,16 @@ function Login() {
     return (
         <div className='sign-up-form'>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Please Enter Your Email</label>
-                <input type="email" className="input-box" placeholder="Your Email"></input>
+                <input type="email" className="input-box" placeholder="Your Email" onChange={(e) => setEmail(e.target.value)}></input>
                 <label>Please Enter Your Password</label>
-                <input type="password" className="input-box" placeholder="Your Password"></input>
-                <button type="button" className="signup-btn">Login</button>
+                <input type="password" className="input-box" placeholder="Your Password" onChange={(e) => setPassword(e.target.value)}></input>
+                <button type="submit" className="signup-btn">Login</button>
                 <hr/>
                 <p>Don't Have an Account? Register Now!</p>
                 <a href={'http://localhost:3000/register'} >
-                        <button onClick type='button' className='signup-btn'>Register</button>
+                        <button type='button' className='signup-btn'>Register</button>
                 </a>
             </form>
         </div>
