d7c57c4d9997c7b47ca2bbd148a60fcca7399c21
Cameron Vong <cameronvong@gmail.com>
2023-04-09 21:08:38 -0400

    Reformat contact page

--- app/src/views/Contact/Contact.js
+++ app/src/views/Contact/Contact.js
@@ -7,10 +7,15 @@ function Contact() {
         <div className='contact'>
             <h2>Contact</h2>
             <text>
-                <a href="https://github.com/cameronvong"> Cameron Vong </a> <br />
-                <a href="https://github.com/Tyarporn"> Ty Arpornsuksant </a> <br />
-                <a href="https://github.com/Shashanka-Sharma"> Shashanka Sharma </a> <br />
-                <a href="https://github.com/chowdhurytahmeed "> Tahmeed Chowdhury </a> <br />
+                <h3>The information of each member of the Coldbrew Coders team is listed below</h3>
+                <p> For issues with backend development, contact                 
+                    <a href="https://github.com/Tyarporn"> Ty Arpornsuksant </a> and
+                    <a href="https://github.com/Shashanka-Sharma"> Shashanka Sharma </a> <br /></p>
+
+                <p> For issues with frontend development, contact
+                    <a href="https://github.com/cameronvong"> Cameron Vong </a> and
+                    <a href="https://github.com/chowdhurytahmeed "> Tahmeed Chowdhury </a> <br /></p>
+
             </text>
         </div>
     );
