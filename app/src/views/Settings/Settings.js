118b21aaa1df482597f674634778c79f5f13f7c5
Cameron Vong <cameronvong@gmail.com>
2023-04-15 23:44:54 -0400

    Added a signout button

--- app/src/views/Settings/Settings.js
+++ app/src/views/Settings/Settings.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import './About.css'
+import './Settings.css'
 
 class About extends Component {
     render() {
         return (
             <div>
-                <h2>ABOUT BREWBOTS</h2>
-                <p>Brewbots is an innovative student project to help manage Discord servers. It is a Discord bot distribution website that allows users to discover and install bots with ease.</p>
-                <p> The platform also features reviews and ratings from other users, providing valuable insights to help users make informed decisions when selecting a bot. </p>
-                <p>With Brewbots, users can take their Discord servers to the next level by adding powerful and useful bots, making it easier to manage their servers and interact with other users.</p>
-                <h2>GOT QUESTIONS?</h2>
-                <p> Please check out our GitHub pages for questions related to the development and running of this project.</p>
+                <h2>SIGN OUT</h2>
             </div>
 
             
