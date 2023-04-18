b34854724a1d5ec1e8149e5d8834ef7fe485718f
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-04-17 23:24:57 -0400

    made settings page view work

--- app/src/views/Settings/Settings.js
+++ app/src/views/Settings/Settings.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import './Settings.css'
 
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <h2>SIGN OUT</h2>
-            </div>
+function Settings() {
 
-            
-        );
-    }
-}
+    return (
+        <div>
+        <h2>SIGN OUT</h2>
+        </div>
+    );
+};
 
 export default Settings;
