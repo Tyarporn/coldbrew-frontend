c2ae865698911ef090332a9795bb59d762c7209a
Cameron Vong <cameronvong@gmail.com>
2023-04-23 21:52:17 -0400

    Added change username to settings
    Still requires backend functionality - hard to test on my machine but working on it

--- app/src/views/Settings/Settings.js
+++ app/src/views/Settings/Settings.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import './Settings.css'
 
 function Settings() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+
     return (
         <div>
-        <h2>SIGN OUT</h2>
+                <label>Please Enter Your New Email</label>
+                <input type="email" className="input-box" placeholder="Your Email" onChange={(e) => setEmail(e.target.value)}></input>
+                <label>Please Enter Your New Password</label>
+                <input type="password" className="input-box" placeholder="Your Password" onChange={(e) => setPassword(e.target.value)}></input>
         </div>
     );
 };
