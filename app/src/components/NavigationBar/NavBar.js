b4dca46234e5dfb61acaa08775081bd65dcf1638
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-04-25 17:04:00 -0400

    modifyed hateoas to add settings view and logout button

--- app/src/components/NavigationBar/NavBar.js
+++ app/src/components/NavigationBar/NavBar.js
@@ -2,14 +2,15 @@ import './NavBar.css'
 import React from 'react'
 import {useState, useEffect} from 'react';
 import axios from "axios"
+import Login from "../../views/Login/Login"
 
 function NavBar() {
     const [mainMenuA, setMainMenuA] = useState([])
     const [mainMenuB, setMainMenuB] = useState([])
     const [mainMenuC, setMainMenuC] = useState([])
     const [mainMenuD, setMainMenuD] = useState([])
-    const [mainMenuE, setMainMenuE] = useState([])
-
+    const [mainMenuF, setMainMenuF] = useState([])
+ 
     const fetchHATEOAS = async () => {
         return axios.get("http://127.0.0.1:8080/main_menu")
         .then(res => {
@@ -18,7 +19,7 @@ function NavBar() {
             setMainMenuB(res.data.Choices.b)
             setMainMenuC(res.data.Choices.c)
             setMainMenuD(res.data.Choices.d)
-            setMainMenuE(res.data.Choices.e)
+            setMainMenuF(res.data.Choices.f)
             
         })
         .catch(err => {
@@ -39,7 +40,8 @@ function NavBar() {
                 <li><a href={mainMenuB.url}>Discover</a></li>
                 <li><a href={mainMenuC.url}>About</a></li>
                 <li><a href={mainMenuD.url}>Contact Us</a></li>
-                <li className='last_button'><a href={mainMenuE.url}>Login</a></li>
+                <li><a href={mainMenuF.url}>Settings</a></li>
+                <li><a href={'http://localhost:3000/login'}>Logout</a></li>
             </ul>
             </div>
         </div>
