6a0f1355cef1b4103f82150494665550dacce3ae
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-04-26 13:43:50 -0400

    changed route in card

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -2,12 +2,14 @@ import './Card.css'
 import React from "react";
 
 function Card({image, name, botDesc, botID}) {
+    // const apiURI = process.env.REACT_APP_LOCALHOST_URL;
+    const apiURI = process.env.REACT_APP_FRONTEND_URL;
     return (
         <div className='card'>
 
             <div className='row product'>
                 <div className="middle-area">
-                    <a href={'http://localhost:3000/' + name} >
+                    <a href={apiURI + name} >
                         <button onClick type='button' className='btn'>{name}</button>
                     </a>
                     <br></br>
